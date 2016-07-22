05e526dcd40addf18a25acb4912d2da6b15b2f0c
Marvki <marvkipi@gmail.com>
2016-07-21 22:08:34 -0700

    authentication hash

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, ENV['my7SVULWHK3cyRluA5nNvpClS'], ENV['	z2bXBCrdDkbQUJIizQJJvpdWzrIttA18zHUg7lZr3lI5J2Aedz']
+  provider :twitter, 'my7SVULWHK3cyRluA5nNvpClS', 'z2bXBCrdDkbQUJIizQJJvpdWzrIttA18zHUg7lZr3lI5J2Aedz'
 end
