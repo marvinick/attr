9c655b72734e9817b48086e1b751fd8f60e4cf28
Marvki <marvkipi@gmail.com>
2016-07-26 06:29:47 -0700

    product model and controller barebones

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160722053523) do
+ActiveRecord::Schema.define(version: 20160726132649) do
+
+  create_table "products", force: :cascade do |t|
+    t.string   "name"
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "provider",   null: false
