2666be2e7e5ea17e3d6d70c3c18d37bf76ecdfa1
Marvki <marvkipi@gmail.com>
2016-08-03 23:50:27 -0700

    crew member resources

--- app/models/starship.rb
+++ app/models/starship.rb
@@ -1,6 +1,5 @@
 class Starship < ApplicationRecord
   has_many :crew_members
-  accepts_nested_attributes_for :crew_members
-  allow_destroy: true
+  accepts_nested_attributes_for :crew_members, allow_destroy: true
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,5 @@ Rails.application.routes.draw do
   delete '/logout', to: 'sessions#destroy'
   resources :charges
   resources :products
+  resources :crew_members, only: [:new, :create]
 end
